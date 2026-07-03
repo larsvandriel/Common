d059e07822c02f97f957dd2d29113e13b8b9349c
lars.van.driel <lars.van.driel@hotmail.com>
2026-07-03 17:18:56 +0200

    Add ASP.NET Core ProblemDetails enricher project
    Introduce a new Common.Results.AspNetCore project (net10.0) with implicit usings and nullable enabled. Adds a skeleton HttpProblemDetailsEnricher that takes an IHttpContextAccessor and a placeholder internal IProblemDetailEnricher interface in Common.Results. This sets up the foundation for enriching ProblemDetails with HTTP context information.

---
+++ CommonLibraries/Common.Results.AspNetCore/HttpProblemDetailsEnricher.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Common.Results.AspNetCore
+{
+    public class HttpProblemDetailsEnricher (IHttpContextAccessor) : IProblemDetailEnricher
+    {
+
+    }
+}
