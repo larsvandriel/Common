4a64243bbe5038f79e1764928946ee5cd5dc8e80
lars.van.driel <lars.van.driel@hotmail.com>
2026-07-07 19:07:46 +0200

    Add ResultExtensions with MapFailure methods
    Add extension methods to map Result failures to different target types. Includes two overloads: one for Result to Result<TTarget>, and another for Result<TSource> to Result<TTarget>. Both methods preserve failure information while enabling type conversion.

---
+++ CommonLibraries/Common.Results/ResultExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Results
+{
+    public static class ResultExtensions
+    {
+        public static Result<TTarget> MapFailure<TTarget>(this Result result)
+        {
+            if (result.IsSuccess)
+                throw new InvalidOperationException("Cannot convert a successful Result to a failure result.");
+
+            return Result<TTarget>.Failure(result.Problem!);
+        }
+
+        public static Result<TTarget> MapFailure<TSource, TTarget>(this Result<TSource> result)
+        {
+            if (result.IsSuccess)
+                throw new InvalidOperationException("Cannot convert a successful Result to a failure result.");
+
+            return Result<TTarget>.Failure(result.Problem!);
+        }
+    }
+}
